84c41520ed40579cd9fcd9def2870d86bd825e5d
prashanta khatri <tokikin21@gmail.com>
2026-07-14 12:26:43 +0545

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ODOO_URL = os.getenv("ODOO_URL", "http://localhost:8069")
-ODOO_DB = os.getenv("ODOO_DB", "hospital_db")
+ODOO_DB = os.getenv("ODOO_DB", "hospi")
 ODOO_USER = os.getenv("ODOO_USER", "admin")
-ODOO_PASSWORD = os.getenv("ODOO_PASSWORD", "admin")
+ODOO_PASSWORD = os.getenv("ODOO_PASSWORD", "a")
 
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")
-WEBHOOK_BASE_URL = os.getenv("WEBHOOK_BASE_URL", "https://your-ngrok.ngrok-free.dev")
+WEBHOOK_BASE_URL = os.getenv("WEBHOOK_BASE_URL", "https://jarring-")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-very-secret-key-change-in-production")
+JWT_ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
